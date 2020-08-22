be03e264df61f0a0083a40c4d4e32a16bebefd22
jcgonzaga <gonzaga.jc1993@gmail.com>
2020-08-22 19:52:01 +0800

    Installed express and initial config

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+if (process.env.NODE_ENV !== "production") {
+  const webpack = require("webpack");
+  const WebpackDevMiddleware = require("webpack-dev-middleware");
+  const webpackConfig = require("./webpack.config.js");
+  app.use(WebpackDevMiddleware(webpack(webpackConfig)));
+} else {
+  app.use(express.static("dist"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist/index.html"));
+  });
+}
+
+app.listen(process.env.port || 8080, () => console.log("Listening"));
